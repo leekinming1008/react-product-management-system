d1b86eda1bdd400abd5cde79dc39b923db5d25e3
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-07 11:29:32 -0500

    Fix the bug for routerprovider tag

--- src/main.tsx
+++ src/main.tsx
@@ -8,23 +8,29 @@ import AddProduct from "./pages/AddProduct";
 import EditProduct from "./pages/EditProduct";
 import Layout from "./pages/Layout";
 
-const browserRouter = createBrowserRouter([{
-  path: "/",
-  element: <Layout />,
-  children:[{
+const browserRouter = createBrowserRouter([
+  {
     path: "/",
-    element: <Home />,
-  },{
-    path: "/addProduct",
-    element: <AddProduct />
-  },{
-    path: "/editProduct/:id",
-    element: <EditProduct />
-  }]
-}]);
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/addProduct",
+        element: <AddProduct />,
+      },
+      {
+        path: "/editProduct/:id",
+        element: <EditProduct />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={browserRouter}>
+    <RouterProvider router={browserRouter} />
   </StrictMode>
 );
