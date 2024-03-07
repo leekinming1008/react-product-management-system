478eede211de865607592998f0d54c5986475c27
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-07 14:21:17 -0500

    Create the validation for the product form

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,11 +1,11 @@
-import { Field, Form, Formik, useFormik } from "formik";
+import { Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 
-const validation = Yup.object({
+const validation = Yup.object().shape({
   name: Yup.string().required("Please enter the product name"),
   price: Yup.number()
     .max(1000)
-    .min(0)
+    .min(1)
     .required("Please enter the product price"),
   description: Yup.string()
     .max(1000)
@@ -22,21 +22,32 @@ const ProductForm = () => {
         price: 0,
         description: "",
       }}
+      validationSchema={validation}
       onSubmit={(values) => {
         console.log(values);
       }}
     >
-      <Form>
-        <p>Image url: </p>
-        <Field name="imageUrl" type="url" />
-        <p>Product name: </p>
-        <Field name="name" type="text" />
-        <p>Price: </p>
-        <Field name="price" type="number" />
-        <p>Description: </p>
-        <Field name="description" type="text" />
-        <button type="submit">Create the new product</button>
-      </Form>
+      {({ errors, touched }) => (
+        <Form>
+          <p>Image url: </p>
+          <Field name="imageUrl" type="url" />
+          {errors.imageUrl && touched.imageUrl ? (
+            <div>{errors.imageUrl}</div>
+          ) : null}
+          <p>Product name: </p>
+          <Field name="name" type="text" />
+          {errors.name && touched.name ? <div>{errors.name}</div> : null}
+          <p>Price: </p>
+          <Field name="price" type="number" />
+          {errors.price && touched.price ? <div>{errors.price}</div> : null}
+          <p>Description: </p>
+          <Field name="description" type="text" />
+          {errors.description && touched.description ? (
+            <div>{errors.description}</div>
+          ) : null}
+          <button type="submit">Create the new product</button>
+        </Form>
+      )}
     </Formik>
   );
 };
