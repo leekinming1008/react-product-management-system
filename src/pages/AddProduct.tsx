b4b17dca3d6ae9fa09d8a0343f63b362eeb8d98b
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-07 12:09:56 -0500

    Add product form to add product page

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -1,5 +1,11 @@
+import ProductForm from "../components/ProductForm";
+
 const AddProduct = () => {
-  return <></>;
+  return (
+    <>
+      <ProductForm />
+    </>
+  );
 };
 
 export default AddProduct;
