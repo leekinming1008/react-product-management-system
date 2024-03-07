a0b3d5c36d97f88b614124d8b183bc29775f67cf
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-07 11:37:56 -0500

    create the add product form

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -1,5 +1,29 @@
+import { Field, Form, Formik } from "formik";
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  return (
+    <>
+      <Formik
+        initialValues={{
+          productName: "",
+          price: 0,
+          description: "",
+          imageUrl: "",
+        }}
+        onSubmit={(values) => {}}
+      >
+        <Form>
+          <p>Image url: </p>
+          <Field name="imageUrl" type="url" />
+          <p>Product name: </p>
+          <Field name="productName" type="text" />
+          <p>Price: </p>
+          <Field name="price" type="number" />
+          <p>Description: </p>
+          <Field name="description" type="text" />
+        </Form>
+      </Formik>
+    </>
+  );
 };
 
 export default AddProduct;
