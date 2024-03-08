80b756410e93b56261ba50da5779c00699181388
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-08 17:32:36 -0500

    Update the nav bar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,118 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 import { Link } from "react-router-dom";
-import { Tab, TabList } from "@chakra-ui/tabs";
-import styled from "styled-components";
-
-const NavbarContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  /* top right bottom left */
-  padding: 10px 20px;
-  background-color: #282c34;
-  color: white;
-`;
-
-const SelectionSection = styled.div`
-  position: absolute;
-  left: 50%;
-`;
 
 const Navbar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
   return (
-    <NavbarContainer>
-      <h1>Angus Inventory</h1>
-      <SelectionSection>
-        <TabList>
-          <Tab>
-            <Link to={"/"}>Home Page</Link>
-          </Tab>
-          <Tab>
-            <Link to={"/addProduct"}>Add Product</Link>
-          </Tab>
-          <Tab>
-            <Link to={"/favorites"}>Favorites</Link>
-          </Tab>
-        </TabList>
-      </SelectionSection>
-    </NavbarContainer>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            ANGUS INVENTORY
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem key="HomePage" onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">HomePage</Typography>
+              </MenuItem>
+              <MenuItem key="AddProduct" onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">AddProduct</Typography>
+              </MenuItem>
+              <MenuItem key="Favorites" onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">Favorites</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Button
+              key="HomePage"
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              <Link to={"/"}>Home Page</Link>
+            </Button>
+            <Button
+              key="AddProduct"
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              <Link to={"/addProduct"}>Add Product</Link>
+            </Button>
+            <Button
+              key="Favorites"
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              <Link to={"/favorites"}>Favorites</Link>
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,16 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import { TabPanels, Tabs } from "@chakra-ui/tabs";
 
 const Layout = () => {
   return (
     <>
-      <Tabs isFitted variant="enclosed">
-        <Navbar />
-        <TabPanels>
-          <Outlet />
-        </TabPanels>
-      </Tabs>
+      <Navbar />
+      <Outlet />
     </>
   );
 };
